d55932a4c152cb78776d1437582e586efad42750
il00li <yl99383899@gmail.com>
2025-08-16 13:33:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // تكوين اتصال قاعدة البيانات
 const pool = new Pool({
